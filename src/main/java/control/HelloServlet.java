a70159c58861068d0ea108ebebe179168ecee279
Michele98770 <michelechierchia2@gmail.com>
2025-04-12 13:12:53 +0200

    initial

---
+++ src/main/java/control/HelloServlet.java
@@ -0,0 +1,26 @@
+package control;
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String message= (String) request.getAttribute("message");
+
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+
+    }
+
+
+}
